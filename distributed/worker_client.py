1d1a15a8ccaae9ff17584a311826e0d4f4c1bb6b
PTCG Agent Dev <ptcg-agent@local>
2026-07-08 01:56:08 +0300

    Hide pyspiel module from sys.modules during kaggle_environments import to prevent C++ initialization warning lists from outputting to console

--- distributed/worker_client.py
+++ distributed/worker_client.py
@@ -33,6 +33,7 @@ class DummyStream:
 @contextlib.contextmanager
 def silence_kaggle_warnings():
     import builtins
+    import sys
     orig_print = builtins.print
     def dummy_print(*args, **kwargs):
         if args and isinstance(args[0], str) and ("Loading environment" in args[0] and "failed" in args[0]):
@@ -40,12 +41,20 @@ def silence_kaggle_warnings():
         orig_print(*args, **kwargs)
     builtins.print = dummy_print
     
+    # Hide pyspiel module from python loader during import to bypass OpenSpiel C++ prints
+    saved_pyspiel = sys.modules.get('pyspiel')
+    sys.modules['pyspiel'] = None
+    
     dummy_stream = DummyStream()
     try:
         with contextlib.redirect_stderr(dummy_stream), contextlib.redirect_stdout(dummy_stream):
             yield
     finally:
         builtins.print = orig_print
+        if saved_pyspiel is not None:
+            sys.modules['pyspiel'] = saved_pyspiel
+        else:
+            sys.modules.pop('pyspiel', None)
 
 def ensure_dependencies():
     # Purge any old colliding test_agents compiled bytecode cache files in root folder
